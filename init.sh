e7618d1426b121f11d36e847ebd5b20698df5e8e
Stefan Popovic <stefan.popovic@humanity.com>
2020-06-30 17:22:00 +0200

    Lock tables

--- init.sh
+++ init.sh
@@ -16,7 +16,8 @@ done
 
 echo "Importing database into master..."
 mysql -uroot -pL0c@lhost --host $DB_HOST --port $MASTER_PORT < dump.db
-#docker exec -i mysql-master -c "mysql -uroot -pL0c@lhost --host $DB_HOST --port $MASTER_PORT" < dump.db
+
+mysql -uroot -pL0c@lhost --host $DB_HOST --port $MASTER_PORT -e "FLUSH TABLES WITH READ LOCK;"
 
 echo "Dumping master database..."
 mysqldump -uroot -pL0c@lhost --host $DB_HOST --port $MASTER_PORT --column-statistics=0 --master-data --databases shiftcom_2010_new availability event_log employee_assignment_log > masterdump.db
@@ -28,7 +29,6 @@ echo "Done importing"
 
 priv_stmt='GRANT REPLICATION SLAVE ON *.* TO "root"'
 mysql -uroot -pL0c@lhost --host $DB_HOST --port $MASTER_PORT -e "$priv_stmt"
-#docker exec -i mysql-master -c "mysql -uroot -pL0c@lhost --host $DB_HOST --port $MASTER_PORT -e '$priv_stmt'"
 
 master_status=$(mysql -uroot -pL0c@lhost --host $DB_HOST --port $MASTER_PORT -e 'SHOW MASTER STATUS' --silent --silent)
 master_status_array=($master_status)
@@ -37,3 +37,5 @@ replication_position=${master_status_array[1]}
 
 mysql -uroot -pL0c@lhost --host $DB_HOST --port $SLAVE_PORT -e "CHANGE MASTER TO MASTER_HOST='$DB_HOST', MASTER_PORT=$MASTER_PORT, MASTER_USER='root', MASTER_PASSWORD='L0c@lhost', MASTER_LOG_FILE='$replication_file', MASTER_LOG_POS=$replication_position;"
 mysql -uroot -pL0c@lhost --host $DB_HOST --port $SLAVE_PORT -e "RESET SLAVE; START SLAVE;"
+
+mysql -uroot -pL0c@lhost --host $DB_HOST --port $MASTER_PORT -e "UNLOCK TABLES;"
